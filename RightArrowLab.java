771656e52010e3307ba8c043123cdb3499e185fc
Tabitha <53835819+TabbyCat444@users.noreply.github.com>
2023-12-29 22:18:39 -0600

    Create RightArrowLab.java

---
+++ RightArrowLab.java
@@ -0,0 +1,22 @@
+// DRAW A RIGHT FACING ARROW USING USER PROVIDED INTEGERS FOR THE ARROW BODY AND HEAD //
+
+import java.util.Scanner; 
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in); 
+      int baseChar; 
+      int headChar;
+      
+      baseChar = scnr.nextInt();
+      headChar = scnr.nextInt();
+      
+      System.out.println("     " + headChar);
+      System.out.println("     " + headChar + headChar);
+      System.out.println("" + baseChar + "" + baseChar + "" + baseChar + "" + baseChar + "" + baseChar + "" + headChar + "" + headChar + "" + headChar);
+      System.out.println("" + baseChar + "" + baseChar + "" + baseChar + "" + baseChar + "" + baseChar + "" + headChar + "" + headChar + "" + headChar + "" + headChar);
+      System.out.println("" + baseChar + "" + baseChar + "" + baseChar + "" + baseChar + "" + baseChar + "" + headChar + "" + headChar + "" + headChar);
+      System.out.println("     " + headChar + headChar);
+      System.out.println("     " + headChar);
+   }
+}
