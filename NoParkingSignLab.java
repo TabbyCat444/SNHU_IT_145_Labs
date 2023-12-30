354130826e2360c82f2d82a5747b34fb20a40c91
Tabitha <53835819+TabbyCat444@users.noreply.github.com>
2023-12-29 22:14:33 -0600

    Create NoParkingSignLab.java

---
+++ NoParkingSignLab.java
@@ -0,0 +1,8 @@
+// A FORMATTED NO PARKING SIGN //
+
+public class LabProgram {
+   public static void main(String[] args) {
+      System.out.println("  NO PARKING");
+      System.out.println("2:00 - 6:00 a.m.");
+   }
+}
